6f2cfd5b1531cf5c20eb052d8d4fe74287522b65
Todd Olsen <todd.olsen@me.com>
2024-12-11 08:26:53 -0500

    Day 1

--- Day1/Package.swift
+++ Day1/Package.swift
@@ -5,10 +5,12 @@ import PackageDescription
 let package = Package(
     name: "Day1",
     platforms: [.macOS(.v15)],
+    dependencies: [.package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0")],
     targets: [
         .executableTarget(
             name: "Day1",
+            dependencies: ["SwiftAlgorithms"],
             resources: [.process("Input")]
-        ),
+        )
     ]
 )
